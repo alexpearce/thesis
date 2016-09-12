a97edc109bbf4cbf80fe03d16414073180a4e7ea
Alex Pearce <alex@alexpearce.me>
2016-09-12 18:21:26 +0200

    First go at results chapter.

---
+++ scripts/copy_external.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Copy plots from places outside this folder
+
+cp ~/Physics/CharmProduction/analysis/xsecplots/output/2015/D0_2015_differential.pdf ../figures/production/results/D0_double_differential.pdf
+cp ~/Physics/CharmProduction/analysis/xsecplots/output/2015/Dp_2015_differential.pdf ../figures/production/results/Dp_double_differential.pdf
+cp ~/Physics/CharmProduction/analysis/xsecplots/output/2015/Dsp_2015_differential.pdf ../figures/production/results/Ds_double_differential.pdf
+cp ~/Physics/CharmProduction/analysis/xsecplots/output/2015/Dstar_2015_differential.pdf ../figures/production/results/Dst_double_differential.pdf
+
+cp ~/Physics/CharmProduction/analysis/xsecplots/output/2015/D0_2015_year_ratio_with_2010.pdf ../figures/production/results/D0_ratio_with_2010.pdf
+cp ~/Physics/CharmProduction/analysis/xsecplots/output/2015/Dp_2015_year_ratio_with_2010.pdf ../figures/production/results/Dp_ratio_with_2010.pdf
+cp ~/Physics/CharmProduction/analysis/xsecplots/output/2015/Dsp_2015_year_ratio_with_2010.pdf ../figures/production/results/Ds_ratio_with_2010.pdf
+cp ~/Physics/CharmProduction/analysis/xsecplots/output/2015/Dstar_2015_year_ratio_with_2010.pdf ../figures/production/results/Dst_ratio_with_2010.pdf
+
+cp ~/Physics/CharmProduction/analysis/xsecplots/output/2015/xsec_integrated.pdf ../figures/production/results/integrated_double_differential_0_8.pdf
+cp ~/Physics/CharmProduction/analysis/xsecplots/output/2015/xsec_integrated_18.pdf ../figures/production/results/integrated_double_differential_1_8.pdf
